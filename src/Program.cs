3dbd70384c9f48b4dd6fe6c6ecdae1fb985c8d7b
Miguel Pérez García <miguelpg_95@hotmail.com>
2017-02-28 21:23:01 -0600

    Migration

--- src/Program.cs
+++ src/Program.cs
@@ -3,52 +3,37 @@ using System.Collections.Generic;
 using System.Linq;
 using System.IO;
 using System.Xml.Linq;
-using System.Threading.Tasks;
 using System.Diagnostics;
 using Newtonsoft.Json;
 
-namespace OSMtoJSON
+namespace src
 {
     class Program
     {
         static void Main(string[] args)
         {
             var stp = new Stopwatch();
-            var listOfTimes = new List<long>();
+
             IEnumerable<Node> nodes;
             List<string> tags = new List<string>();
 
-            using (var file = new StreamReader(args[0]))
+            stp.Start();
+            using(var file = new StreamReader(File.OpenRead(args[0])))
             {
-                stp.Start();
                 var xml = XDocument.Load(file);
-                stp.Stop();
 
-                listOfTimes.Add(stp.ElapsedMilliseconds);
-                Console.WriteLine($"took {stp.ElapsedMilliseconds} Milliseconds to load file");
-                stp.Reset();
-
-                stp.Start();
                 nodes = xml.Descendants()
                             .AsParallel()
                             .Where(node => node.Name.LocalName == "node")
-                            .Select(node => new Node
-                            {
+                            .Select(node => new Node{
                                 id = node.Attribute("id").Value,
                                 lat = node.Attribute("lat").Value,
                                 lon = node.Attribute("lon").Value,
                                 tags = node.Elements()
-                                            .Select(innerTags => new Tuple<string, string>(innerTags.Attribute("k").Value, innerTags.Attribute("v").Value))
+                                            .Select(innerTags => new Tuple<string,string>(innerTags.Attribute("k").Value, innerTags.Attribute("v").Value))
                                             .ToDictionary(tuple1 => tuple1.Item1, tuple2 => tuple2.Item2)
                             });
-                stp.Stop();
-
-                listOfTimes.Add(stp.ElapsedMilliseconds);
-                Console.WriteLine($"took {stp.ElapsedMilliseconds} Milliseconds to build IEnumerable<Node>");
-                stp.Reset();
             }
-
-            Console.WriteLine($"Found {nodes.Count()} nodes");
 
             foreach (var node in nodes)
             {
@@ -59,26 +44,14 @@ namespace OSMtoJSON
             }
 
             var distinctTags = tags.Distinct();
-            foreach (var tag in distinctTags)
-            {
-                Console.WriteLine(tag);
-            }
-
+            File.WriteAllLines(@".\tags.txt", distinctTags);
 
             var json = JsonConvert.SerializeObject(nodes);
+            File.WriteAllText(@".\map.json", json);
+            stp.Stop();
 
-            Directory.CreateDirectory(@"C:\OutputMaps\");
-            File.WriteAllText(@"C:\OutputMaps\map.json", json);
-
-            long totalTime = 0;
-            foreach (var time in listOfTimes)
-            {
-                totalTime += time;
-            }
-
-            Console.WriteLine($"Total time {TimeSpan.FromMilliseconds(totalTime)}");
-
-            Console.ReadLine();
+            Console.WriteLine($"Finished in {stp.Elapsed}");
+            
         }
 
         class Node
